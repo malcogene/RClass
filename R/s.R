3f9e1c8dfb4d5a9b37be0114e84bba85d80da6de
palelamp <palelamp@snu.ac.kr>
2020-05-31 09:48:56 +0900

    sdf

--- R/s.R
+++ R/s.R
@@ -6,7 +6,7 @@
 #' @usage AppUI()
 #' @param sfPower numerical value of ... for
 #' @return A user interface will be shown in users' default web browser.
-#' @import shiny        
+#' @import shiny ggplot2 ggrepel limma ComplexHeatmap        
 #' @importFrom limma plotMDS
 #' @export 
 AppUI <- function() {
@@ -15,3 +15,152 @@ AppUI <- function() {
   shiny::runApp(system.file("shiny", package=pkgname))} #
 appU <- function() { loc <- gsub('.*:', '', getAnywhere("AppUI")$where[1]) 
   shiny::runApp(system.file("shiny", package=loc))  }
+
+
+options(stringsAsFactors = FALSE)
+
+is.installed <- function(RequiredPackages) {
+  for (i in RequiredPackages) { # Installs packages if not yet installed
+    if (!is.element(i, installed.packages()[,1]))  install.packages(i)
+    require(i, character.only = T)
+  }
+}
+
+is.installed.bioconductor <- function(RequiredPackages) {
+  for (i in RequiredPackages) { # Installs packages if not yet installed
+    if (!is.element(i, installed.packages()[,1])) {
+      source("http://bioconductor.org/biocLite.R")
+      biocLite(i)  
+      require(i, character.only = T)
+    }
+  }
+}
+
+
+loadUrl <- function(url, sep=c("RData"," ", "," , "\t", ";", "xls", "gsheet"), ...) {
+  cat('onedrive: copy link\n googlesheet: share-> Anyone with the link\n sep: "RData", ..."xls", "gsheet"\n')
+  
+  tmpFile <- tempfile()
+  url2 <- gsub("e=.*", "download=1", url)
+  download.file(url2,tmpFile)
+  sep <- match.arg(sep)
+  if(sep == "RData") {
+    justLoaded <- try(load(tmpFile), silent = T); 
+    try(assign(justLoaded, eval(as.symbol(justLoaded)),.GlobalEnv ), silent = T);
+    if(class(justLoaded)=="try-error"){ justLoaded <- try(read.delim(tmpFile, ...), silent = T); message("Need 'sep' argument, is it txt file?")  }   
+  } else if(sep == "xls") {
+    is.installed('readxl')
+    justLoaded <- try(read_excel(tmpFile,...), silent = T)
+    
+  } else if(sep == "gsheet") {
+    is.installed('gsheet')
+    cat('gsheet should be public, click share-> Anyone with the link')
+    justLoaded <- gsheet2tbl(url,...)
+  } else {
+    justLoaded <- try(read.delim(tmpFile, sep=sep, ...), silent = T)  
+  }
+  justLoaded 
+}
+
+browseEntrez <- function(entrezIDs) {
+  for(i in entrezIDs) {
+    browseURL(paste0("https://www.ncbi.nlm.nih.gov/gene/", i))
+  }
+}
+
+
+
+peep <- function(x, boxplot = F ) { 
+  if(is.null(dim(x))) { if(length(x) > 10)  { print(x[1:10]) } else { print(x) }  } else if (dim(x)[1] >=10 && dim(x)[2]>=5 ){ print(x[1:5, 1:3]); boxplot(x[1:5, 1:3]) } else {print(head(x)); boxplot(x)} }
+
+
+
+normalize.q <- function(x= data.frame(matrix(sample(12, replace = T), 4)), filter.sd.quantile = 0.1, tied = c("average", "min", "max"), verbose = T ) {
+  # compare to normalize.quantiles, 1. accept data.frame 2. tie control option:"average", "min", "max"  3. sd.filter 4. peep & plot & verbose...
+  
+  x <- x[rowSums(x)>0, ]  
+  x <- x[apply(x,1,sd) >= quantile(apply(x,1,sd), filter.sd.quantile), ]  
+  cat(sprintf("\nrowSums(x)==0, =<quantile(sd(row), %s) were filtered\n\n", filter.sd.quantile))
+  
+  tied <- match.arg(tied)  
+  rank <- apply(x, 2, rank,ties.method="min"); 
+  if(any(tied %in% c("average", "max"))) rank.max <- apply(x, 2, rank,ties.method="max"); 
+  sorted <- apply(x, 2, sort)
+  sorted.row.mean <- apply(sorted, 1, mean); 
+  x2 <- apply(rank, 2, function(x) sorted.row.mean[x])
+  if(any(tied %in% c("average", "max"))) x2.max <- apply(rank.max, 2, function(x) sorted.row.mean[x])
+  if(tied=="average") { x2 <- (x2+x2.max)/2 } else if (tied=="max"){x2 <- x2.max } else { }
+  
+  if( class(x) == "data.frame") { x2 <- as.data.frame(x2); rownames(x2) <- rownames(x) }
+  if(verbose) {
+    op <- par(no.readonly = T); par(mfrow=c(1,2), mar=c(3,3,1,1))
+    cat('Original matrix or data.frame\n'); peep(x, T)
+    cat('Sort the original matrix from lowest to highest\n'); peep(rank)
+    cat('Determine the ranks of original matix\n');peep(sorted)
+    cat('\nCalculate the means\n\n'); peep(sorted.row.mean)
+    cat('\n\nFinally substitute the means into our ranked matrix\n'); peep(x2, T)
+    cat(sprintf('If the values were tied, %s is used\n\n', tied))
+    par(op)
+    'In the example on Wikipedia, if the values were tied, the min value is used but in the normalize.quantiles() function, the average is used'
+  }
+  x2
+}
+
+
+
+
+
+DEGs <- function(Exp, cl, adj.pval = 0.1,  logFC = 2, geomTextN=5, heatmapUpN = 25, plotDEG =T, multipleRegression=F, rowCounts=F, meanFilter=10, PDF=T, cname='temp' ) {
+  try(dev.off(), silent = T)
+ # is.installed(c('ggplot2', 'ggrepel'))
+ # is.installed(c('limma', 'ComplexHeatmap'))
+  
+  if(rowCounts) { Exp <- Exp[apply(Exp, 1, mean) > meanFilter, ]; Exp <- voom(Exp, plot = T) }
+  
+  if(multipleRegression) { fit <-eBayes(lmFit(Exp, model.matrix(~ .,cl))); print(topTable(fit, 2))                                     } else {
+    fit <-eBayes(lmFit(Exp, model.matrix(~ .,cl[, 1, drop=F]))); print(topTable(fit, 2)) } 
+  
+  tT <- topTable(fit, number = dim(Exp)[1])
+  tT$Gene <- rownames(tT)
+  tT.up <- tT[order(tT$logFC, decreasing = T ),]; tT.down<- tT[order(tT$logFC),]
+  tT.filter <- data.frame(tT[inx<-(tT$adj.P.Val<adj.pval) & (abs(tT$logFC) > logFC ), ]); print(tT.filter)
+  
+  if(PDF) {
+    pdf(file = file.path(getwd(),sprintf("%s.pdf", cname )), width=5, height=5)  }
+  if(plotDEG) {
+    if(any(colnames(tT) == "logFC" && dim(tT.filter)[1] != 0) ) {
+      require(ggplot2); require(ggrepel)
+      tT$Cutoff_value <- c("Not Sig", sprintf("FDR < %s & logFC > %s", adj.pval, logFC))[as.numeric(inx)+1]
+      gplot <- ggplot(tT, aes(x = logFC, y = -log10(adj.P.Val))) + geom_point(aes(color = Cutoff_value)) + labs(title ="c") + scale_color_manual(values = c("red", "grey")) +  theme_bw(base_size = 12) + theme(legend.position = "bottom") + geom_hline(yintercept= -log10(adj.pval), linetype="dashed", color = "#FF000050") + geom_vline(xintercept= c(logFC, -logFC), linetype="dashed", color = "#FF000050") 
+      
+      g <- gplot + geom_text_repel(    
+        data = dd <- rbind(tT.up[1:geomTextN, ], tT.down[1:geomTextN, ]),
+        aes(label = Gene),
+        size = 3,
+        box.padding = unit(0.35, "lines"),
+        point.padding = unit(0.3, "lines")
+      ) }
+    print(g)
+    
+    if(dim(Exp)[1] >= heatmapUpN*2 ) {
+      bluered <- colorRampPalette(c("blue", "white", "red"))(256)
+      # stats::heatmap(Exp[rbind(tT.down[1:heatmapUpN, ],tT.up[heatmapUpN:1, ])$Gene,], col = bluered, scale = "row", main = sprintf("top%s", heatmapUpN*2), Colv = NA, Rowv=NA    )
+      
+      # if(HUGO) { colnames(d)[1:dim(matGS)[1]] <- .mapid(colnames(d)[1:dim(matGS)[1]]) }
+      
+      colse=c("#00000020", "#000000", "#0000BF10", "#0000BF30", "#0000BF50", "#0000BF70","#0000BF90","#0000BF")
+      colTemp <- colse[as.numeric(as.factor(cl[,1]))]
+      names(colTemp ) <- cl[,1]
+      colTemp<-list(colTemp); names(colTemp) <- colnames(cl)[1] 
+      
+      h <- Heatmap( t(scale(t(d<-Exp[rbind(tT.up[1:heatmapUpN, ], tT.down[heatmapUpN:1, ])$Gene,]))),  col = bluered, name="Exprs", rect_gp = gpar(col = "gray12", lty = 1, lwd = 0.2), cluster_rows = T, cluster_columns = T, show_row_names = T, row_names_gp =gpar(fontsize = 5),   split = data.frame(cyl = factor(c(rep('UP', heatmapUpN), rep('DOWN', heatmapUpN)), levels=c('UP','DOWN' ))),gap = unit(1.5, "mm"), top_annotation = HeatmapAnnotation(df=cl, col=colTemp, show_annotation_name = T, annotation_name_gp = gpar(fontsize = 7), annotation_name_side ='left' ), annotation_height=unit(8, "mm")  ) 
+      pre.h<<-h
+      draw(h)
+    }
+  }
+  if(PDF) { dev.off() 
+  }
+  return(list(fit = fit, tT.filter= tT.filter, tT.up=tT.up, tT.down=tT.down))
+}
+
+
